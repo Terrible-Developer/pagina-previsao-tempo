7f5d2f67225b0e22eee45668c1abfb30ec600a9a
Victor <victorbparo@gmail.com>
2020-02-02 11:53:22 -0300

    retirando comentarios e logs desnecessarios

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,24 +7,13 @@ formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     resultado.textContent = 'Carregando Requisição. Por Favor Aguarde';
     const nome = nomeBusca.value;
-    console.log(nome);
     fetch('/busca?nomeLocal=' + nome).then((resposta) => {
-    console.log(resposta);
     resposta.json().then((dados) => {
         if(dados.erro){
             resultado.innerText = dados.erro;
         }else{
-            console.log(dados);
             resultado.innerText = dados.local + '\n' + dados.previsao;
         }
     });
  });
 });
-
-// fetch('http://localhost:9000/pagode?nomeLocal=umuarama').then((resposta) => {
-//     resposta.json().then((dados) => {
-//         console.log(dados);
-//         texto1.innerText = dados.previsao;
-//         texto2.innerText = dados.local;
-//     });
-// });
